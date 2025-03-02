e9838e5eb48fec74a803e1fc034bdd0955d692aa
Abhinav <jha.abhinav2710@gmail.com>
2025-03-02 14:10:02 +0530

    feat: Completed Process Management and Finalised the Initial Process Schematics.

--- internal/process/manager.go
+++ internal/process/manager.go
@@ -7,3 +7,73 @@ type Manager struct {
 func NewManager() *Manager {
 	return &Manager{}
 }
+
+func (m *Manager) AddProcess(p *Process) {
+	m.Processes = append(m.Processes, p)
+}
+
+func (m *Manager) RemoveProcess(pid int) {
+	for i, p := range m.Processes {
+		if p.Pid == pid {
+			m.Processes = append(m.Processes[:i], m.Processes[i+1:]...)
+			break
+		}
+	}
+}
+
+func (m *Manager) GetProcess(pid int) *Process {
+	for _, p := range m.Processes {
+		if p.Pid == pid {
+			return p
+		}
+	}
+	return nil
+}
+
+func (m *Manager) GetProcesses() []*Process {
+	return m.Processes
+}
+
+func (m *Manager) StartProcess(name, cmd string, restart bool) {
+	p := NewProcess(name, cmd, restart)
+	p.StartProcess()
+	m.AddProcess(p)
+}
+
+func (m *Manager) StopProcess(pid int) {
+	p := m.GetProcess(pid)
+	if p != nil {
+		p.StopProcess()
+	}
+}
+
+func (m *Manager) RestartProcess(pid int) {
+	p := m.GetProcess(pid)
+	if p != nil {
+		p.RestartProcess()
+	}
+}
+
+func (m *Manager) GetProcessStatus(pid int) string {
+	p := m.GetProcess(pid)
+	if p != nil {
+		return p.Status
+	}
+	return ""
+}
+
+func (m *Manager) GetProcessCPUUsage(pid int) float64 {
+	p := m.GetProcess(pid)
+	if p != nil {
+		return p.CPUUsage
+	}
+	return 0
+}
+
+func (m *Manager) GetProcessMemory(pid int) float64 {
+	p := m.GetProcess(pid)
+	if p != nil {
+		return p.MemoryUsage
+	}
+	return 0
+}
--- internal/process/process.go
+++ internal/process/process.go
@@ -1,21 +1,62 @@
 package process
 
+import (
+	"os/exec"
+	"time"
+
+	"github.com/google/uuid"
+)
+
 type Process struct {
-	ID       string
-	Cmd      string
-	Args     []string
-	Pid      int
-	Status   string
-	Restart  bool
-	CPUUsage float64
-	Memory   float64
+	ID          string
+	Pid         int
+	Name        string
+	Cmd         string
+	Restart     bool
+	Status      string
+	CPUUsage    float64
+	MemoryUsage float64
+	CmdProcess  *exec.Cmd
 }
 
-func NewProcess(id, cmd string, args []string) *Process {
-	return &Process{
-		ID:     id,
-		Cmd:    cmd,
-		Args:   args,
-		Status: "stopped",
+func NewProcess(name, cmd string, restart bool) *Process {
+	id := uuid.New().String()
+	if name == "" {
+		name = "process" + "_" + id[:4]
 	}
+	return &Process{
+		ID:          id,
+		Name:        name,
+		Cmd:         cmd,
+		Status:      "stopped",
+		CmdProcess:  nil,
+		CPUUsage:    0,
+		MemoryUsage: 0,
+		Restart:     restart,
+	}
+}
+
+func (p *Process) StartProcess() {
+	proc := exec.Command(p.Cmd)
+	if err := proc.Start(); err != nil {
+		panic(err)
+	}
+	p.CmdProcess = proc
+	p.Pid = proc.Process.Pid
+	p.Status = "running"
+}
+
+func (p *Process) StopProcess() {
+	if p.CmdProcess != nil && p.Status == "running" {
+		if err := p.CmdProcess.Process.Kill(); err != nil {
+			panic(err)
+		}
+		p.Status = "stopped"
+	}
+}
+
+func (p *Process) RestartProcess() {
+	p.StopProcess()
+	time.Sleep(1 * time.Second)
+	p.StartProcess()
 }
