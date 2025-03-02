f83833a823eeb30bab21a6eeebc885998e598a73
Abhinav <jha.abhinav2710@gmail.com>
2025-03-02 19:40:42 +0530

    feat: Fixed The Stop Command Method.

--- cmd/stop.go
+++ cmd/stop.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
@@ -9,17 +11,28 @@ var stopCmd = &cobra.Command{
 	Aliases: []string{"kill"},
 	Short:   "Stop a process",
 	Long:    "Stop a process with the given ID",
-	Args:    cobra.ExactArgs(1),
+	Args:    cobra.MaximumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		_name := cmd.Flag("name").Value.String()
+		if len(args) == 0 && _name == "" {
+			fmt.Println("Please provide an ID or name.")
+			cmd.Help()
+			return
+		}
 		_ID := args[0]
-		if _ID == "" {
-			if _name != "" {
-			} else {
-				cmd.Help()
-			}
-		} else {
+		if _ID != "" {
 			manager.StopProcess(_ID)
+			fmt.Println("Process stopped with ID: ", _ID)
+			return
+		}
+		if _name != "" {
+			for _, p := range manager.GetProcesses() {
+				if p.Name == _name {
+					manager.StopProcess(p.ID)
+					fmt.Println("Process stopped with ID: ", p.ID)
+					return
+				}
+			}
 		}
 	},
 }
